4898a68ca7feb9cfeace4ed8b8880581f2f98c0a
root <root@KHADB.localdomain>
2018-02-23 00:17:05 -0800

    first commit

---
+++ getData.py
@@ -0,0 +1,111 @@
+import writeHTML
+import time
+import json
+import datetime
+import string
+import unicodedata
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+#Function: write data to json format
+def writeToJSON(fileName,data):
+    with open(fileName, 'w') as output:
+        json.dump(data, output)
+
+#format to valid filename
+#Reference: https://gist.github.com/wassname/1393c4a57cfcbf03641dbc31886123b8
+def clean(name, replace=' '):
+    whitelist = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    # replace spaces
+    for r in replace:
+        name = name.replace(r,'_')
+    
+    # keep only valid ascii chars
+    cleaned = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode()
+    
+    # keep only whitelisted chars
+    return ''.join(c for c in cleaned if c in whitelist)
+
+#Function: strip data
+def getData(browser, buildingName):
+    #Select Facilities Filter
+    facilitiesSelect = Select(browser.find_element_by_id('ctl00_pc_Facilities_ddl'))
+    facilitiesSelect.select_by_visible_text(buildingName)
+
+    time.sleep(1) #time
+
+    #Select Room Filter
+    roomSelect = Select(browser.find_element_by_id('ctl00_pc_Rooms_ddl'))
+    roomList = roomSelect.options
+
+    for index, room in enumerate(roomList[1:]):
+        eventInfo = []
+
+        roomName = clean(room.text)
+        
+        print('-------- %s --------' %(roomName))
+
+        time.sleep(1) #time
+
+        #Select room
+        roomSelect.select_by_visible_text(room.text)
+
+        time.sleep(1) #time
+
+        #click "Apply"
+        browser.find_element_by_id("ctl00_pc_ApplyFilterButton").click()
+
+        time.sleep(1) #time
+        
+        #Strip data from room
+        table = browser.find_element_by_id('ctl00_pc_ListViewGrid')
+        rows = table.find_elements_by_tag_name('tr')
+        for row in rows[2:]:
+            obj = row.find_elements_by_tag_name('td')
+            
+            startTime = obj[0].text
+            endTime = obj[1].text
+            eventName = obj[2].text
+            client = obj[3].text
+            info = {'startTime':startTime,
+                    'endTime':endTime,
+                    'eventName':eventName,
+                    'client':client}
+
+            eventInfo.append(info)
+
+            print('%s %s %s %s' %(startTime, endTime, eventName, client))
+       
+        writeHTML.writeToHTML(roomName,eventInfo)
+        
+        time.sleep(1) #time
+            
+        #Select Filter
+        browser.find_element_by_id('ctl00_pc_FilterViewButton').click()
+
+if __name__== '__main__':
+    options = Options()
+    options.add_argument("--disable-infobars")
+    browser = webdriver.Chrome(chrome_options=options)
+    url = 'http://vems.oit.uci.edu/MedicalEducation/BrowseEvents.aspx'
+    browser.get(url)
+
+##    #Select view as Monthly List
+##    browser.find_element_by_id('tab2-tab').click()
+##    #Select view By Location
+##    browser.find_element_by_xpath('//*[@id="groupByOptions"]/label[2]').click()
+    
+    #Select Filter
+    browser.find_element_by_id("ctl00_pc_FilterViewButton").click()
+
+    time.sleep(2) #time
+
+    getData(browser, 'SOM Medical Education')
+    getData(browser, 'College of Health Sciences')
+    print('++++DONE++++')
+
+    #close the browser window
+    browser.quit()
---
+++ writeHTML.py
@@ -0,0 +1,130 @@
+import string
+
+def writeToHTML(room, data):
+    l = ['Classroom_1_B002','Classroom_2_B052','Classroom_3_B048',
+                    'Classroom_4_B044','Classroom_5_B040','Classroom_6_B032',
+                    'Classroom_7_B028','Classroom_8_B024','Classroom_10_B016',
+                    'Classroom_3056','Colloquium_3070','Conference_3131',
+                    'Conference_A1000','Conference_B1034','Nelson_Auditorium',
+                    'Patio','Tamkin_F108','Tamkin_F110','Tamkin_F114',
+                    'Telemedicine_Theater_B001','UCIMC_B22A_Room_2112_',
+                    'Med_Surge_II_Multi-Labs_A_B_C']
+    roomList = ['Classrom 1', 'Classrom 2', 'Classrom 3', 'Classrom 4',
+                    'Classrom 5', 'Classrom 6', 'Classrom 7', 'Classrom 8',
+                    'Classrom 10', 'Classrom 3056', 'Colloquium 3070',
+                    'Conference 3131','Conference A1000','Conference B1034',
+                    'Nelson Auditorium','Patio','Tamkin F108','Tamkin F110',
+                    'Tamkin F114','Telemedicine_Theater','UCIMC Room 2112',
+                    'Med Surge II']
+    img = '<img src="src/gif.gif" alt="Have a Good Day!" class="center">' if not bool(data) else ''
+    fileName = room + '.html'
+    roomName = ''
+    for i,r in enumerate(l):
+        if room == r:
+            roomName = roomList[i]
+            break
+
+    html_str = '''<!DOCTYPE HTML>
+        <html lang='eng'>
+
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <meta name='description' content='EMS Schedule'>
+            <meta name='author' content='MedEdIT'>
+            <meta name='keywords' content='EMS Schedule Display'>
+
+            <title>EMS Schedule</title>
+
+            <link rel='stylesheet' href='src/style.css' type='text/css'>
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/json2html/1.0.0/json2html.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.json2html/1.0.0/jquery.json2html.min.js"></script>
+
+            <link href='http://fonts.googleapis.com/css?family=Great+Vibes' rel='stylesheet' type='text/css'>
+			
+			<script>
+				$(document).ready(function () {{
+					var days = [
+					'Sunday',
+					'Monday',
+					'Tuesday',
+					'Wednesday',
+					'Thursday',
+					'Friday',
+					'Saturday'];
+					
+					var months = ["January", "February", "March", "April", "May", "June",
+							   "July", "August", "September", "October", "November", "December"];
+					
+					var currentdate = new Date();
+					var datetime = days[currentdate.getDay()] + ", " + 
+						months[currentdate.getMonth()] + " " + currentdate.getDate() + ", " +
+						currentdate.getFullYear();
+					$('#date').text(datetime)
+				}});
+			</script>
+			
+            <nav>
+                <div id="logo"> <img src="src/UCI-School-of-Medicine.jpg" alt="UCI MEDED"></div>
+                <div id="room-name"> {0} </div>
+                <div id="date">Wed Feb 21 2018</div>
+            </nav>
+            <script>
+                var t = {{
+                    "item": [{{
+                        "<>": "div",
+                        "class": "row",
+                        "html": [{{
+                                "<>": "div",
+                                "class": "col-time",
+                                "html": [{{
+                                        "<>": "div",
+                                        "class": "start-time",
+                                        "html": "${{startTime}}"
+                                    }},
+                                    {{
+                                        "<>": "div",
+                                        "class": "end-time",
+                                        "html": "${{endTime}}"
+                                    }}
+                                ]
+                            }},
+                            {{
+                                "<>": "div",
+                                "class": "col-info row-event",
+                                "html": "${{eventName}}"
+                            }}
+                        ]
+                    }}]
+                }};
+
+                var main = {{
+                    "list": {{
+                        "<>": "div",
+                        "id": "container",
+                        "html": function() {{ 
+                            return (json2html.transform(d, t.item))
+                        }}
+                    }}
+                }};
+
+                var d = {1};
+
+                document.write(json2html.transform({{}}, main.list));
+            </script>
+
+        </head>
+
+        <body>
+            {2}
+        </body>
+
+        </html>'''.format(roomName, data, img)
+
+    html_file = open(fileName, 'w')
+    html_file.write(html_str)
+    html_file.close()
+
